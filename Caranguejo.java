3fc0fd5e69563dab72671b89e05cb8281ccdd349
Julio César Alves <julio.caburu@gmail.com>
2023-10-23 15:15:24 -0300

    Versão inicial

---
+++ Caranguejo.java
@@ -0,0 +1,14 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class Caranguejo extends Actor
+{
+    /**
+     * Act - faz o que o Caranguejo queira fazer.
+     * Este método é chamado sempre que o botão 'Executar' ou
+     * 'Executar uma vez' é chamado no ambiente.
+     */
+    public void act() 
+    {
+        
+    }    
+}
---
+++ MundoDoCaranguejo.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Escreva aqui uma descrição da classe MundoDoCarganguejo.
+ * 
+ * @author (seu nome) 
+ * @version (um número de versão ou uma data)
+ */
+public class MundoDoCaranguejo extends World
+{
+
+    /**
+     * Construtor para objetos da classe MundoDoCaranguejo.
+     * 
+     */
+    public MundoDoCaranguejo()
+    {    
+        super(560, 560, 1); 
+    }
+}
